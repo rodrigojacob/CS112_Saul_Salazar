140053dc79d1e5c1c6ebca770d56eb10883e383e
rodrigojacob <rodrigosaul@sbcglobal.net>
2013-10-08 11:42:55 -0700

    Welcome4
    Exercises

---
+++ Welcome4.java
@@ -0,0 +1,15 @@
+//SAC CS112
+//Saul Salazar
+//Displaying Text with Printf
+
+public class Welcome4 //Declaring Class
+{
+
+	public static void main(String[] args)//declaring Method
+	{
+	System.out.printf( "%s\n%s\n%s\n",
+			"Welcome Students", "to the", "Java Programing Class");// Body one statement
+
+	}//End of Method
+
+}// End of Class
