27ecfcf53baa4e621e0dec0c57ca8e7ecaf728ca
rodrigojacob <rodrigosaul@sbcglobal.net>
2013-10-08 11:57:00 -0700

    Welcome3
    Exercises

---
+++ Welcome3.java
@@ -0,0 +1,13 @@
+//SAC CS112 Java Programming
+//Saul Salazar
+//Displaying multiple lines of Text with a simple Statement
+public class Welcome3 //Declaring Class
+{
+
+	public static void main(String[] args)// Declaring Method
+	{
+		System.out.println ("Welcome\nStudents\nto\nthe\nJava\nProgramming\nClass!");//Body of the Method 
+
+	}//End of Method
+
+}//End of Class
