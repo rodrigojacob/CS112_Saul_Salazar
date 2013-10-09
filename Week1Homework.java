c7aa75b85d24f0a59922de55b3eb2379187c19d3
rodrigojacob <rodrigosaul@sbcglobal.net>
2013-10-09 16:32:36 -0700

    Week1Homework
    Exercises java programming class

---
+++ Week1Homework.java
@@ -0,0 +1,17 @@
+/*SAC CS 112 Java Programming Fall 2013
+Saul Salazar
+Printing a Line of Text "Welcome Students to Java Programming Class"*/
+//Declaring the class
+
+public class Week1Homework
+{
+
+	// Declaring a Main Method
+	
+	public static void main(String[] args)
+	{
+	//Performing Output
+System.out.println( "Welcome Students to the Java Programming Class 2013!");
+	}//End of the Method
+
+}//End of Class Welcome1
