d8e4341cd2e7de273191a4132c9d0264dd569d01
rodrigojacob <rodrigosaul@sbcglobal.net>
2013-10-10 14:49:08 -0700

    Week2Lab3
    IDEs pros and cons.

---
+++ Week2Lab3.java
@@ -0,0 +1,41 @@
+//SAC CS112 Java Programming
+//Saul Salazar
+//IDEs
+
+public class Week2Lab3 //Declaring class
+{
+public static void main(String [] args)//Declaring Method
+{//Statements
+System.out.println("Name two Java IDes.");//question 1
+
+System.out.println("Eclipse and NetBeans.");
+
+System.out.println("");//Blank space
+
+System.out.println("List two pros and 2 cons for each.");//question 2
+
+System.out.println("");//Blank space
+
+System.out.println("Eclipse Pros:");
+
+System.out.println("- Has a huge array of plugins.\n- Deliver capavilities no available in other IDEs.");
+
+System.out.println("");//Blank space
+
+System.out.println("Eclipse Cons");
+
+System.out.println("- It does not have a pleasing and intuitive interface compare to other IDEs.\n- A lot can go wrong as the number of plugins increases.");
+
+System.out.println("");//Blank space
+
+System.out.println("NetBean Pros:");
+
+System.out.println("- Easier to navigate.\n- Good for beginner Java developers");
+
+System.out.println("");//Blank space
+
+System.out.println("NetBean Cons");
+
+System.out.println("- Dificult to grap at an advance level.\n- Limited Plugins.");
+}//end of Method
+}//End of Class
