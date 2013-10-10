587327d464cc922447fc82d632f2f492034dccec
rodrigojacob <rodrigosaul@sbcglobal.net>
2013-10-10 12:16:21 -0700

    Week2Lab2
    Question about Variable.

---
+++ Week2Lab2.java
@@ -0,0 +1,26 @@
+//SAC CS112 Java Programming
+//Saul Salazar
+//questions about Variable
+
+public class Week2Lab2 //Declaring class
+{
+public static void main(String [] args)//Declaring Method
+{//Statements
+System.out.println("What is a variable?");//question 1
+
+System.out.println("a variable is a storage location and an associated\nsymbolic name which contains some known or unknown\nquantity or information, a value.");
+
+System.out.println("");//Blank space
+
+System.out.println("How a variable relate to memory?");//question 2
+
+System.out.println("For the allocation and representation of their values.\nA variable is bound to the address of some particular\nblock (contiguous sequence) of bytes in memory.");
+
+System.out.println("");//Blank space
+
+System.out.println("How is a variable relate to hexadecimal, binary?");// question 3
+
+System.out.println("Binary and hexadecimal are just different ways of\nrepresenting the number in a more human-friendly way.");
+
+}//end of Method
+}//End of Class
