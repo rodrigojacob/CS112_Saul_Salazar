07d8a0a785eb6b463abc652ad4e6a58c6d832853
rodrigojacob <rodrigosaul@sbcglobal.net>
2013-10-10 15:24:38 -0700

    Week2Lab4
    Questions web and web server.

---
+++ Week2Lab4.java
@@ -0,0 +1,19 @@
+//SAC CS112 Java Programming
+//Saul Salazar
+//Questions about Software technologies.
+
+public class Week2Lab4 //Declaring class
+{
+public static void main(String [] args)//Declaring Method
+{//Statements
+System.out.println("What is web programming?.");//question 1
+
+System.out.println("Web programming provides a computing plataform\nfor developing and running applications as a\nservice over the web.");
+
+System.out.println("");
+
+System.out.println("What is web server?.");//question 2
+
+System.out.println("Web server is software that runs on servers\nelsewhere on the internet.");
+}//end of Method
+}//End of Class
