b6e1779a4a196f695a622d930856d54602576937
rodrigojacob <rodrigosaul@sbcglobal.net>
2013-10-10 15:38:24 -0700

    Week2Lab5
    Upload your HelloWorld app

---
+++ Week2Lab5.java
@@ -0,0 +1,18 @@
+/*SAC CS 112 Java Programming class 2013.
+Saul Salazar
+*/
+//Lab O5 Upload you Hello World app.
+//Declaring the class
+
+public class Week2Lab5
+{
+
+	// Declaring a Main Method
+	
+	public static void main(String[] args)
+	{
+	//Performing Output
+System.out.println( "HelloWorld!");
+	}//End of the Method
+
+}//End of Class Welcome1
