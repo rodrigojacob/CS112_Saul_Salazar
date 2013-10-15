330b77d3b476f58daef3bdb5bb6fa3544567c285
rodrigojacob <rodrigosaul@sbcglobal.net>
2013-10-14 17:40:33 -0700

    Week3Lab2
    Write a program to print
    Hello
    My name is "your name goes here"

---
+++ Week3Lab2.java
@@ -0,0 +1,16 @@
+//SAC CS112 Java Programming Class 2013
+//Saul Salazar
+//Write a Program to print "Hello
+//My name is "your name goes here"
+
+public class Week3Lab2
+
+{
+	public static void main (String[] args){
+		//statements
+		System.out.println("Hello,");
+		System.out.print("My name is ");
+		System.out.println("\"Saul Salazar\"");
+		
+	}//end method
+}//end class
