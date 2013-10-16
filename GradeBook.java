c87531e42b0f196f6d55e64ede3fb9ee84c39b7d
rodrigojacob <rodrigosaul@sbcglobal.net>
2013-10-15 19:42:33 -0700

    Week8Lab1
    Declaring a class with a Method and Instantiating an Object of a Class

---
+++ GradeBook.java
@@ -0,0 +1,12 @@
+//Chapter 3: Fig.3.1.
+//Class declaration with one method.
+
+//Class
+public class GradeBook
+{
+//Display a welcome message to the GradeBook user.
+	public void displayMessage()
+	{
+		System.out.println("Welcome to the Grade Book!" );
+	}
+}
