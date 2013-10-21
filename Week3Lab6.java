ba38ba18d2e2c625904ab27ad21bb3b6418cc14d
rodrigojacob <rodrigosaul@sbcglobal.net>
2013-10-21 09:37:34 -0700

    Week3Lab6
    Write a program to read 1 number and check to see if that number is
    greater than 10 or less than 5.

---
+++ Week3Lab6.java
@@ -0,0 +1,20 @@
+//SAC CS112 Introduction to Java Programming.
+//Saul Salazzar
+/*Write a program to Read 1 number and 
+check to see if number is greater than 10 of less than 5*/
+
+public class Week3Lab6//Declaring Class.
+{
+public static void main ( String arg[])//Declaring Method.
+{
+int x =	4;//variable.
+
+if (x > 10)//if statement.
+{
+	System.out.print("X is greater than 10");//expression 1
+	}
+if ( x < 5 )
+{System.out.print("X is less than 5.");//expression 2
+}
+}
+}
